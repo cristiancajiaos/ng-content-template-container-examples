be2e6d03190a00e360cad4b459a1400a2c39ef6b
Cristian Cajiao <cristiancajiaos@outlook.com>
2022-04-21 12:29:25 -0400

    Creados componentes para usar ng-content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './layout/layout.component';
+import { ErrorComponent } from './error/error.component';
+import { WarningComponent } from './warning/warning.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LayoutComponent
+    LayoutComponent,
+    ErrorComponent,
+    WarningComponent
   ],
   imports: [
     BrowserModule,
